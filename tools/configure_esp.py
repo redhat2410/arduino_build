a6a3d6c6cfc31c814378a8af5123a2c8b8ce32ff
redhat2410 <ducvuminhspk@gmail.com>
2020-05-06 08:25:36 +0700

    Upload by Ducvu

--- tools/configure_esp.py
+++ tools/configure_esp.py
@@ -140,6 +140,10 @@ def process(source):
             pathLibrary = path.join(root, folder)
             break
 
+    pathLibrary = pathLibrary + "\\packages\\esp8266\\hardware\\esp8266\\2.4.0\\libraries"
+
+    print(pathLibrary)
+
     search(source)
     searchPath()
 
