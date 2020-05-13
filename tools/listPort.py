63b9d95f00bb741f5221a591f75c2372d79a24b8
redhat2410 <ducvuminhspk@gmail.com>
2020-05-13 11:11:56 +0700

    Upload by Ducvu

--- tools/listPort.py
+++
@@ -1,9 +0,0 @@
-import serial.tools.list_ports
-
-ports = serial.tools.list_ports.comports()
-
-print("********")
-for port, desc, hwid in sorted(ports):
-    print("* " + port + " *")
-
-print("********")
