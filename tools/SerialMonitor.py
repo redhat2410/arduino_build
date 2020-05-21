6615d532f47fb936dfa4d44de6a9bdeeb20a3a16
redhat2410 <ducvuminhspk@gmail.com>
2020-05-21 15:48:16 +0700

    Upload by Ducvu

--- tools/SerialMonitor.py
+++ tools/SerialMonitor.py
@@ -3,11 +3,8 @@ import serial
 port = input("Enter port name: ")
 baud = input("Enter baudrate : ")
 
-try:
-    ser = serial.Serial(port, baud)
+ser = serial.Serial(port, baud)
 
-    while 1:
-        data = ser.read()
-        print(data)
-except:
-    print("An exception occurred")
+while 1:
+    data = ser.read()
+    print(data)
