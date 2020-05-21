956e97d183e275a1a37349676dfdc3f001202ef6
redhat2410 <ducvuminhspk@gmail.com>
2020-05-21 17:05:44 +0700

    Upload by Ducvu

--- tools/SerialMonitor.py
+++ tools/SerialMonitor.py
@@ -1,24 +1,16 @@
 import serial
 import sys
+import time
 
-try:
-    port = input("Enter port name: ")
-    baud = input("Enter baudrate : ")
+port = input("Enter port name: ")
+baud = input("Enter baudrate : ")
 
+ser = serial.Serial(port, baud, timeout=1)
+
+while True:
     try:
-        ser = serial.Serial(port, baud)
-    except serial.serialutil.SerialException:
-        print("Name port is wrong")
+        data = ser.read().decode('ascii')
+    except:
         sys.exit()
-
-    while 1:
-        try:
-            data = ser.readline().decode('ascii')
-        except KeyboardInterrupt:
-            sys.exit()
-
-        if data != "":
-            print(data)
-except KeyboardInterrupt:
-    print("\nProgram is exit\n")
-    sys.exit(0)
+    if data != "":
+        print(data)
