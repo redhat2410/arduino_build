909470728d780a15bce2706df97215c1f405eaed
redhat2410 <ducvuminhspk@gmail.com>
2020-05-21 15:42:20 +0700

    Upload by Ducvu

--- tools/SerialMonitor.py
+++ tools/SerialMonitor.py
@@ -1,13 +1,31 @@
 import serial
+import sys
 
 port = input("Enter port name: ")
 baud = input("Enter baudrate : ")
 
-try:
-    ser = serial.Serial(port, baud)
+# try:
+#     # ser = serial.Serial(port, baud, timeout=1 )
 
-    while 1:
-        data = ser.read()
+#     while True:
+#         k = cv2.waitKey(1) & 0xFF
+#         # data = ser.readline().decode('ascii')
+#         # if data != "" :
+#         #     print(data)
+#         print(k)
+#         if k == ord('q'):
+#             print("quit")
+#             break
+
+# except:
+#     print("An exception occurred")
+
+ser = serial.Serial(port, baud, timeout = 1)
+while True:
+    try:
+        data = ser.readline().decode('ascii')
+    except:
+        sys.exit()
+
+    if data != "":
         print(data)
-except:
-    print("An exception occurred")
